9c352bf73e691295d0206bb7d866a7715aee81f8
Otavin23 <alvesotavio132@gmail.com>
2022-07-09 13:34:17 -0300

    just missing the responsive summer

---
+++ src/@types/index.ts
@@ -0,0 +1,9 @@
+
+
+export interface IAdvice{
+    slip: {
+        advice: string,       
+        id: number,
+    }
+
+}
--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,37 +1,37 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { IAdvice } from "../../@types"
 import * as S from "./style"
 
-interface IAdvice{
-    slip: {
-        id: number,
-        advice: string 
-    }
-}
 
 
 const Main = ()=>{
 
-    const [advice, setAdvice] = useState<IAdvice[]>([])
-
+    const [advice, setAdvice] = useState<IAdvice>()
     
     async function getAdvice() {
-        const adviceGet = await axios.get("https://api.adviceslip.com/advice")
-        console.log(adviceGet)
-    }
-
-
+        const { data } = await axios.get<IAdvice>("https://api.adviceslip.com/advice")
+        setAdvice(data)
+    }  
 
     return (
         <S.MainBg>
             <S.MainContainer>
                 <div className="adviceID">
-                    <p>ADVICE  #117</p>
+                    {advice?.slip.id ? (
+                        <p>ADVICE #{advice.slip.id}</p>
+                    ): (
+                        <p>ADVICE #117</p>
+                    )}
                 </div>
                 <div className="resultText">
-                    <h1>"It is easy to sit up and take notice
-                        , what's difficult is getting 
-                        up and talking action"</h1>
+                    {advice?.slip.advice ? (
+                        <h1>"{advice?.slip.advice}"</h1>
+                    ): (
+                        <h1>"It ts easy to sit up and take
+                            notice, what's difficult is gretting
+                            up and taking action"</h1>
+                    )}
                 </div>
                 <div className="fotter-row">
                     <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
